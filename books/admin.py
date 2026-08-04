cc916554a3bb22c5acec23dbab71941978eafa11
paria <pariaydgr@gmail.com>
2026-08-04 20:05:46 +0330

    feat(books): add admin registration and API routes

--- books/admin.py
+++ books/admin.py
@@ -1,3 +1,6 @@
-from django.contrib import admin
 
-# Register your models here.
+from django.contrib import admin
+from .models import Book, Favorite
+
+admin.site.register(Book)
+admin.site.register(Favorite)

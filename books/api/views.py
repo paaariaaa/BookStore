f16e3cf82e3af5cea8a82b3fded58a0f68962111
paria <pariaydgr@gmail.com>
2026-08-05 17:30:07 +0330

    feat(books): implement authenticated favorites API

--- books/api/views.py
+++ books/api/views.py
@@ -1,18 +1,46 @@
-from rest_framework.generics import ListAPIView ,RetrieveAPIView
-from books.models import Book,Favorite
-from .serializers import BookSerializer
-from rest_framework.permissions import IsAuthenticated
+from django.db.models import BooleanField, Exists, OuterRef, Value
+from django.shortcuts import get_object_or_404
 from rest_framework import status
+from rest_framework.generics import ListAPIView, RetrieveAPIView
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
+
+from books.models import Book, Favorite
+
+from .serializers import BookSerializer
+
+
+def with_favorite_status(queryset, user):
+    if not user.is_authenticated:
+        return queryset.annotate(
+            _is_favorite=Value(False, output_field=BooleanField()),
+        )
+
+    return queryset.annotate(
+        _is_favorite=Exists(
+            Favorite.objects.filter(
+                user=user,
+                book_id=OuterRef("pk"),
+            )
+        )
+    )
+
+
 class BookListView(ListAPIView):
-    queryset = Book.objects.all()
     serializer_class = BookSerializer
 
+    def get_queryset(self):
+        return with_favorite_status(Book.objects.all(), self.request.user)
+
+
 class BookDetailView(RetrieveAPIView):
-	queryset = Book.objects.all()
-	serializer_class = BookSerializer
+    serializer_class = BookSerializer
+
+    def get_queryset(self):
+        return with_favorite_status(Book.objects.all(), self.request.user)
+
 
 class FavoriteListView(ListAPIView):
     serializer_class = BookSerializer
@@ -20,27 +48,36 @@ class FavoriteListView(ListAPIView):
 
     def get_queryset(self):
         return Book.objects.filter(
-            favorites__user=self.request.user
+            favorites__user=self.request.user,
+        ).annotate(
+            _is_favorite=Value(True, output_field=BooleanField()),
         )
+
 
 class BookFavoriteView(APIView):
     permission_classes = [IsAuthenticated]
 
-    def put(self, request, pk):
-        book = Book.objects.get(pk=pk)
-
-        Favorite.objects.get_or_create(
+    def post(self, request, pk):
+        book = get_object_or_404(Book, pk=pk)
+        _, created = Favorite.objects.get_or_create(
             user=request.user,
             book=book,
         )
 
         return Response(
-            {"is_favorite": True},
-            status=status.HTTP_200_OK,
+            {
+                "book_id": book.pk,
+                "is_favorite": True,
+            },
+            status=(
+                status.HTTP_201_CREATED
+                if created
+                else status.HTTP_200_OK
+            ),
         )
 
     def delete(self, request, pk):
-        book = Book.objects.get(pk=pk)
+        book = get_object_or_404(Book, pk=pk)
 
         Favorite.objects.filter(
             user=request.user,
